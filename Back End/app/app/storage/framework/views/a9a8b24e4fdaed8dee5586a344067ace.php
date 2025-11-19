e96d2a2ffbabb13fa730ccde6aad1feab46a71f2
tienuxxm <156863650+tienuxxm@users.noreply.github.com>
2025-11-19 10:57:22 +0700

    new

---
+++ Back End/app/app/storage/framework/views/a9a8b24e4fdaed8dee5586a344067ace.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Mail</title>
+</head>
+<body>
+    <h1>Xin chào <?php echo e($data['name']); ?></h1>
+    <p>Đây là email test</p>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\app\resources\views/emails/test.blade.php ENDPATH**/ ?>
---
+++ Back End/app/app/storage/framework/views/e4857d2f85c5fa3e854aa4c242f2a938.php
@@ -0,0 +1,6 @@
+<b>Kính gửi: Anh/Chị, </b><br/>
+<p>Đã đặt hàng thành công đơn hàng <?php echo e($data['data']); ?></p><br/>
+
+<b>Lưu ý:</b>
+<p>Đây là email tự động, vui lòng không trả lời email này. Nếu cần hỗ trợ liên hệ qua email: it.support@bitex.com.vn</p>
+<?php /**PATH C:\xampp\htdocs\app\resources\views/emails/order_success.blade.php ENDPATH**/ ?>
